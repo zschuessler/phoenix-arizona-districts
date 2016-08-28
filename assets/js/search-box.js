3502c7da1e95b4624456750adea433cdc804b020
Zachary Schuessler <zlschuessler@gmail.com>
2016-08-28 15:06:19 -0700

    initial commit pre-refactor

---
+++ assets/js/search-box.js
@@ -0,0 +1,10 @@
+var $ = jQuery;
+
+function SearchBox() {
+    this.dom = {
+        $searchInput: $('.address-input')
+    };
+    this.google
+}
+
+module.exports = new SearchBox;
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+
+KML, Shapefile, et al:
+http://maps.phoenix.opendata.arcgis.com/datasets/8b2fdcd4a88b491ebb9cb51b9a59ba17_0
+
